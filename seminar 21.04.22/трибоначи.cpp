0a8ef6587d297f7e63e70efcaaf4d0c0058799e0
GavrichenkoKate <91049308+GavrichenkoKate@users.noreply.github.com>
2022-04-22 11:44:21 +0300

    Update трибоначи.cpp

--- seminar 21.04.22/трибоначи.cpp
+++ seminar 21.04.22/трибоначи.cpp
@@ -1,19 +1,19 @@
 #include <iostream> 
 using namespace std;
  
-int f(int n) 
+int T(int n) 
 {
     if ( n == 0 ) return (0); 
     if ( n == 1 ) return (1); 
     if ( n == 2 ) return (1); 
-    return f(n-1) + f(n-2) + f(n-3);
+    return T(n-1) + T(n-2) + T(n-3);
 }
  
 int main()
 {
     int n;    
     cout << "n="; cin >> n;       
-    cout <<f(n)<<" ";    
+    cout <<T(n)<<" ";    
     cout <<endl;
 system("pause");
 return 0;
